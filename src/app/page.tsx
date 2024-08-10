de285515bb7aeda9536604f08cef9d0f81ba0718
shaun14-del <64382742+shaun14-del@users.noreply.github.com>
2024-08-10 12:26:57 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import UserCard from "./components/UserCard"
 export default async function Home() {
   const session = await getServerSession(options)
 
-  return (
+  return ( 
     <>
       {session ? (
-        <UserCard user={session?.user} pagetype={"Home"} />
+        <UserCard user ={session?.user} pagetype={"Home"} />
       ) : (
-        <h1 className="text-5xl">You Shall Not Pass!</h1>
+        <h1 className="text-5xl">Welcome to uiucGPT</h1>
       )}
     </>
   )
